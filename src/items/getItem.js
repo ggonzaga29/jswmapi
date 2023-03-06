e6ac8fc3678bde69424d6ce2cb3a34f43a9f7eca
Gian Gonzaga <gonzagagianfrancesco@gmail.com>
2023-03-06 19:07:35 +0800

    change wrong @param docs

--- src/items/getItem.js
+++ src/items/getItem.js
@@ -5,7 +5,7 @@ const config = require("../api.config");
  Gets info about an item based on the item name
  @async
  @function getItem
- @param {string} [item] - url_name of an item (required). Example: "Mirage Prime" should be "mirage_prime"
+ @param {string} [item] - url_name of an item (required). Example: "Mirage Prime Blueprint" should be "mirage_prime_blueprint"
  @param {string} [platform='pc'] - Return content for specified platform, default: pc. Available values : xbox, pc, ps4, switch
  @returns {Promise<Array>} - A Promise that resolves to an array of item objects
  @throws {Error} - If the API request fails or returns an error
