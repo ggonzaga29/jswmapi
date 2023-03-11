c8582ed5bc896d1589c4950d1efeb363212948a6
Gian Gonzaga <gonzagagianfrancesco@gmail.com>
2023-03-11 10:24:47 +0800

    add lich and sister

--- src/interfaces/ResponsePayload.ts
+++ src/interfaces/ResponsePayload.ts
@@ -1,8 +1,13 @@
 import {AllItems, Dropsource, Item, Order} from "./item";
+// for sister and lich
+import {Quirk, Weapon, Ephemera} from "./sharedInterfaces";
 
 export default interface ResponsePayload {
     items?: AllItems[];
     dropsources?: Dropsource[];
-    item: Item,
-    orders: Order[];
+    item?: Item,
+    orders?: Order[];
+    quirks?: Quirk[];
+    weapons?: Weapon[];
+    ephemeras?: Ephemera[];
 }
